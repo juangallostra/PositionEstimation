344d5e3d476c5f0f9f490afdc6a3751b71db1fec
Juan Gallostra <jgallostra@naralabs.com>
2018-07-24 13:19:54 +0200

    Begin work on the estimator

--- src/position.cpp
+++ src/position.cpp
@@ -1,3 +1,20 @@
 /*
     position.cpp: Position estimation from optical flow
 */
+
+# include "position.h"
+
+PositionEstimator::PositionEstimator(float KOpticalFlow)
+{
+  this->KOpticalFlow = KOpticalFlow;
+}
+
+void PositionEstimator::angularCompensation(float[2] flow, float[3] gyro, float height)
+{
+
+}
+
+void PositionEstimator::estimate()
+{
+
+}
--- src/position.h
+++ src/position.h
@@ -1,3 +1,20 @@
 /*
     position.h: Position estimation from optical flow
 */
+
+# pragma once
+
+class PositionEstimator {
+
+    private:
+        // Estimator parameters
+        float KOpticalFlow;
+
+        void angularCompensation(float[2] flow, float[3] gyro, float height);
+
+    public:
+        PositionEstimator(float KOpticalFlow);
+        
+        void estimate();
+
+} // class PositionEstimator
